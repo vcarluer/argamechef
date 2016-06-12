d9a09a8d7efffcf5beddd96590fb66afda1bc7d3
Vincent Carluer <vcarluer@gmail.com>
2016-06-12 22:53:57 +0000

    intro

--- js/intro.js
+++ js/intro.js
@@ -1,18 +1,31 @@
+var jingle;
+var concTO;
+
 function load() {
-    var jingle = document.getElementById('jingle');
-    jingle.addEventListener('canplay', function() {
-       jingle.play();
-       setTimeout(function() {
+    jingle = document.getElementById('jingle');
+    
+    jingle.addEventListener('ended', function() {
+        clearTimeout(concTO);
+       window.location.href = 'applications.html'; 
+    });
+}
+
+function go() {
+    jingle.play();
+    var play = document.getElementById('play');
+    play.className += " FadeOut";
+    document.getElementById('title').className += " MoveLeft";;
+    
+    setTimeout(function() {
             document.getElementById('t1').className += " FadeIn";
             document.getElementById('t2').className += " FadeIn";
             document.getElementById('t3').className += " FadeIn";    
        }, 1000);
        
-    });
     
-    jingle.addEventListener('ended', function() {
-       window.location.href = 'applications.html'; 
-    });
+    concTO = setTimeout(function() {
+            window.location.href = 'applications.html'; 
+       }, 10000);
 }
 
 ready(load);
